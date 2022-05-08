ffd7877408328bf8c2695a0a70c95f9ebc3092eb
Wolff2b <86543817+Wolff2b@users.noreply.github.com>
2022-05-08 12:49:38 -0400

    e

--- src/Components/successPath.js
+++ src/Components/successPath.js
@@ -7,7 +7,7 @@ const ErrorPath = () => {
     const msg = query.get("msg");
     return (
         <Grid item>
-            <h1 style={{textAlign: "center", color: "#00e676"}}>Success!</h1>
+            <h1 style={{textAlign: "center", color: "#00e676"}}>Your ban appeal has been successfully submitted!</h1>
             <h4>{msg}</h4>
         </Grid>
     )
